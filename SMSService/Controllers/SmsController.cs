b55527263718097991f0c82d5c04afcc08878d28
Alexandros Balafas <abalafas@ubitech.eu>
2023-02-21 23:17:53 +0200

    update controller

--- SMSService/Controllers/SmsController.cs
+++ SMSService/Controllers/SmsController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SMSService.Context;
 using SMSService.DbContexts;
 using SMSService.Dtos;
+using SMSService.Entities;
 using SMSService.Interfaces;
+using SMSService.Services;
 
 namespace SMSService.Controllers
 {
@@ -11,21 +15,34 @@ namespace SMSService.Controllers
     {
         #region Members
         private ISmsService _smsService;
+        private ISmsVendorStrategy _vendorStrategy;
         private ILogger<SmsController> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly Dictionary<string, ISmsVendorStrategy> _strategies;
 
         #endregion
 
         #region Constructor
-        public SmsController(ISmsService smsService,
+        public SmsController(ISmsService smsService, ISmsVendorStrategy vendorStrategy,
             ILogger<SmsController> logger,
-            ApplicationDbContext context)
+            ApplicationDbContext context, IMapper mapper)
         {
             _smsService = smsService;
+            _vendorStrategy = vendorStrategy;
             _context = context;
             _logger = logger;
+            _mapper = mapper;
         }
         #endregion
+        public SmsController()
+        {
+            _strategies = new Dictionary<string, ISmsVendorStrategy>
+            {
+                { "30", new SmsVendorGRStrategy() },
+                { "35", new SmsVendorCYStrategy() }
+            };
+        }
 
         [HttpGet]
         [Route("getAll")]
@@ -37,11 +54,13 @@ namespace SMSService.Controllers
         }
 
         [HttpPost]
+
         public async Task<IActionResult> SendSms(SmsDto model)
         {
             try
             {
                 var request = await _smsService.SendSmsService(model);
+                //_context.
                 return Ok(request);
             }
             catch (Exception ex)
@@ -50,6 +69,57 @@ namespace SMSService.Controllers
             }
         }
 
+        [HttpPost]
+        [Route("strategy")]
+        public async Task<SmsDto> SendSmsStrategy(SmsDto model)
+        {
+            try
+            {
+                var request = await _vendorStrategy.SendSms(model.Receiver,model.Text);
+                var mod = new Sms()
+                {
+                   Text = model.Text,
+                   Receiver = model.Receiver,
+                   Vendor = request.Vendor,
+                   TimeStamp= DateTime.Now,
+                };
+                await _context.Sms.AddAsync(mod);
+                await _context.SaveChangesAsync();
 
+                var recordDto = _mapper.Map<SmsDto>(mod);
+                return recordDto; 
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void SendSmsWithStrategy(SmsDto model)
+        {
+            try
+            {
+                var messageLang = GetVendorCode(model.Receiver);
+
+                if (_strategies.TryGetValue(messageLang, out var strategy))
+                {
+
+                    var mod = strategy.SendSms(model.Receiver, model.Text);
+
+
+                    Console.WriteLine(mod);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static string GetVendorCode(string number)
+        {
+            return number.Substring(1, 2);
+        }
     }
 }
